77b8e474272b771e950e64d7315380212f0f7f5f
mayconcorreatinti <maycon326754@gmail.com>
2025-11-18 20:55:40 -0300

    corrigindo problema no HTTPException

--- pcr/routers/users.py
+++ pcr/routers/users.py
@@ -1,23 +1,26 @@
 from fastapi import APIRouter,HTTPException
 from pcr.models.users import User,UserResponse
 from pcr.database import Mysqldb
+from http import HTTPStatus
 from pcr.security import hash
 
 
 app = APIRouter(tags=["users"],prefix="/users")
 db = Mysqldb()
 
-@app.post('/')
+@app.post('/',response_model = UserResponse)
 async def register_user(account:User):
     user = await db.select_user_from_table(account.username,account.email)
     if user:
         if user["username"] == account.username:
             raise HTTPException(
-                detail="This name already exists!",
+                detail = "This name already exists!",
+                status_code = HTTPStatus.CONFLICT
             )
         elif user["email"] == account.email:
             raise HTTPException(
-                detail="This email already exists!"
+                detail = "This email already exists!",
+                status_code = HTTPStatus.CONFLICT
             )
     
     await db.insert_user_from_table(
